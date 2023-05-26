f0027d66a1a4bf8e3f48af15edb109bfa0c6861c
Guillem257 <guillem2525@gmail.com>
2023-05-26 09:17:53 +0200

    GMR_Ulti

--- Shinobi - NoFurryGames/Shinobi/ModulePlayer.cpp
+++ Shinobi - NoFurryGames/Shinobi/ModulePlayer.cpp
@@ -331,7 +331,7 @@ update_status ModulePlayer::Update()
 
 		if (ultiEspadaAnim.HasFinished()) {
 
-			App->particles->AddParticle(App->particles->ultiEspada, position.x, position.y - currentAnimation->GetCurrentFrame().h + 12, fPoint(10, -10), Collider::Type::PLAYER_SWORD_ULTI, 0);
+			/*App->particles->AddParticle(App->particles->ultiEspada, position.x, position.y - currentAnimation->GetCurrentFrame().h + 12, fPoint(10, -10), Collider::Type::PLAYER_SWORD_ULTI, 0);
 			App->particles->AddParticle(App->particles->ultiEspada, position.x, position.y - currentAnimation->GetCurrentFrame().h + 12, fPoint(0, -10), Collider::Type::PLAYER_SWORD_ULTI, 0);
 			App->particles->AddParticle(App->particles->ultiEspada, position.x, position.y - currentAnimation->GetCurrentFrame().h + 12, fPoint(-10, -10), Collider::Type::PLAYER_SWORD_ULTI, 0);
 			App->particles->AddParticle(App->particles->ultiEspada, position.x, position.y - currentAnimation->GetCurrentFrame().h + 12, fPoint(5, -10), Collider::Type::PLAYER_SWORD_ULTI, 0);
@@ -340,6 +340,9 @@ update_status ModulePlayer::Update()
 			App->particles->AddParticle(App->particles->ultiEspada, position.x, position.y - currentAnimation->GetCurrentFrame().h + 12, fPoint(-15, -10), Collider::Type::PLAYER_SWORD_ULTI, 0);
 			App->particles->AddParticle(App->particles->ultiEspada, position.x, position.y - currentAnimation->GetCurrentFrame().h + 12, fPoint(-15, -5), Collider::Type::PLAYER_SWORD_ULTI, 0);
 			App->particles->AddParticle(App->particles->ultiEspada, position.x, position.y - currentAnimation->GetCurrentFrame().h + 12, fPoint(15, -5), Collider::Type::PLAYER_SWORD_ULTI, 0);
+			*/
+			App->particles->AddParticle(App->particles->ultiTornado, position.x, 10, fPoint(0, 0), Collider::Type::PLAYER_SWORD_ULTI, 0);
+			
 			if (!App->boss->IsEnabled()) {
 				App->enemy->KillAll();
 			}
