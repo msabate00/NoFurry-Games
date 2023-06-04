8fcd999534f47f963b321f98a93f9d43c080ebcf
ReifeyCode <reifeycontacto@gmail.com>
2023-06-04 17:02:29 +0200

    CODE - More bugs

--- Shinobi - NoFurryGames/Shinobi/Enemy.cpp
+++ Shinobi - NoFurryGames/Shinobi/Enemy.cpp
@@ -137,15 +137,6 @@ void Enemy::Update()
 		if (sword && facingLeft)
 		{
 			App->particles->AddParticle(App->particles->enemySwordL, position.x, position.y - currentAnim->GetCurrentFrame().h + 85, Collider::Type::ENEMY_SWORD, 0);
-			if (swordSwap > 0)
-			{
-				swordSwap--;
-				if (swordSwap == 0)
-				{
-					App->particles->AddParticle(App->particles->enemySwordR, position.x, position.y - currentAnim->GetCurrentFrame().h + 85, Collider::Type::ENEMY_SWORD, 0);
-					swordSwap = 50;
-				}
-			}
 		}
 
 		else if (sword && !facingLeft)
@@ -194,7 +185,7 @@ void Enemy::OnCollision(Collider* c1, Collider* c2)
 		if (facingLeft || currentAnim == &walkBasic)
 		{
 			jumpsNow = true;
-			position.y -= 8;
+			position.y -= 5;
 		}
 	}
 	if (c2->type == Collider::Type::BOX_HELP_RIGHT)
